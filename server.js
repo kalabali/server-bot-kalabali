a01d3794a2edeaece4441f51381d569058f7b026
Teofilus Candra <teofiluschandra@gmail.com>
2018-09-14 11:35:27 +0800

    add reply message

--- server.js
+++ server.js
@@ -12,9 +12,30 @@ const config = {
 const client = new Line.Client(config)
 
 Router.post('/callback', async(ctx) => {
-    ctx.body = "Callback"
+    Promise
+    .all(ctx.request.body.events.map(handleEvent))
+    .then((result) => ctx.body = result)
+    .catch((err) => {
+      console.error(err);
+      ctx.status = 500;
+    });
+    
     console.log('oke')
 })
+
+// event handler
+function handleEvent(event) {
+    if (event.type !== 'message' || event.message.type !== 'text') {
+      // ignore non-text-message event
+      return Promise.resolve(null);
+    }
+  
+    // create a echoing text message
+    const echo = { type: 'text', text: event.message.text };
+  
+    // use reply API
+    return client.replyMessage(event.replyToken, echo);
+  }
 
 app.use(Line.middleware(config))
 app.use(Router.routes())
