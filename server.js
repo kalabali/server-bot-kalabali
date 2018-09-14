e9aa506d5ffb0eed064fd73ea6a5275859241c51
Teofilus Candra <teofiluschandra@gmail.com>
2018-09-14 18:01:59 +0800

    try to add sasih

--- server.js
+++ server.js
@@ -43,7 +43,7 @@ function handleEvent(event) {
     }
 
     if(event.message.text == 'hari ini') {
-        const details = await request.get(`https://kalender-bali.herokuapp.com/v1/details?bulan=9&tahun=2018&tanggal=14`)
+        const details = request.get(`https://kalender-bali.herokuapp.com/v1/details?bulan=9&tahun=2018&tanggal=14`)
         const echo = { type: 'text', text: res.body.details.sasih }
         return client.replyMessage(event.replyToken, echo);
     }
