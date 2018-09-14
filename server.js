15e1d7606038bbd7c7186508d121d9dce4dc2547
Teofilus Candra <teofiluschandra@gmail.com>
2018-09-14 11:26:37 +0800

    add line chatbot

--- server.js
+++ server.js
@@ -1,14 +1,22 @@
 const Koa = require('koa')
 const bodyParser = require('koa-bodyparser')
 const Router = require('koa-router')()
+const Line = require('@line/bot-sdk')
 
 const app = new Koa()
+const config = {
+    channelAccessToken : process.env.channelAccessToken || "",
+    channelSecret : process.env.channelSecret || "" 
+}
+
+const client = new Line.Client(config)
 
 Router.post('/callback', async(ctx) => {
     ctx.body = "Callback"
     console.log('oke')
 })
 
+app.use(Line.middleware(config))
 app.use(Router.routes())
 app.use(bodyParser())
 app.listen(process.env.PORT || 3000)
