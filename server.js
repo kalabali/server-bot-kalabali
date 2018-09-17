83b333fcff244dcf5be882c292013ebeef9985ae
Teofilus Candra <teofiluschandra@gmail.com>
2018-09-17 14:27:24 +0800

    add bubble

--- server.js
+++ server.js
@@ -37,37 +37,106 @@ Router.post('/callback', async(ctx) => {
                     type: 'template',
                     altText: 'Confirm alt text',
                     template: {
-                        {
-                            "type": "bubble",
-                            "styles": {
-                              "footer": {
-                                "separator": true
-                              }
+                        "type": "bubble",
+                        "styles": {
+                          "footer": {
+                            "separator": true
+                          }
+                        },
+                        "body": {
+                          "type": "box",
+                          "layout": "vertical",
+                          "contents": [
+                            {
+                              "type": "text",
+                              "text": "Kalender Bali",
+                              "weight": "bold",
+                              "color": "#1DB446",
+                              "size": "sm"
                             },
-                            "body": {
+                            {
+                              "type": "text",
+                              "text": "17 September 2018",
+                              "weight": "bold",
+                              "size": "xl",
+                              "margin": "md"
+                            },
+                            {
+                              "type": "text",
+                              "text": "Events : Tilem",
+                              "size": "xs",
+                              "color": "#aaaaaa",
+                              "wrap": true
+                            },
+                            {
+                              "type": "separator",
+                              "margin": "xxl"
+                            },
+                            {
                               "type": "box",
                               "layout": "vertical",
+                              "margin": "xxl",
+                              "spacing": "sm",
                               "contents": [
                                 {
-                                  "type": "text",
-                                  "text": "Kalender Bali",
-                                  "weight": "bold",
-                                  "color": "#1DB446",
-                                  "size": "sm"
+                                  "type": "box",
+                                  "layout": "horizontal",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Energy Drink",
+                                      "size": "sm",
+                                      "color": "#555555",
+                                      "flex": 0
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "$2.99",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
+                                    }
+                                  ]
                                 },
                                 {
-                                  "type": "text",
-                                  "text": "17 September 2018",
-                                  "weight": "bold",
-                                  "size": "xl",
-                                  "margin": "md"
+                                  "type": "box",
+                                  "layout": "horizontal",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Chewing Gum",
+                                      "size": "sm",
+                                      "color": "#555555",
+                                      "flex": 0
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "$0.99",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
+                                    }
+                                  ]
                                 },
                                 {
-                                  "type": "text",
-                                  "text": "Events : Tilem",
-                                  "size": "xs",
-                                  "color": "#aaaaaa",
-                                  "wrap": true
+                                  "type": "box",
+                                  "layout": "horizontal",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "Bottled Water",
+                                      "size": "sm",
+                                      "color": "#555555",
+                                      "flex": 0
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "$3.33",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
+                                    }
+                                  ]
                                 },
                                 {
                                   "type": "separator",
@@ -75,162 +144,91 @@ Router.post('/callback', async(ctx) => {
                                 },
                                 {
                                   "type": "box",
-                                  "layout": "vertical",
+                                  "layout": "horizontal",
                                   "margin": "xxl",
-                                  "spacing": "sm",
                                   "contents": [
                                     {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Energy Drink",
-                                          "size": "sm",
-                                          "color": "#555555",
-                                          "flex": 0
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "$2.99",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
+                                      "type": "text",
+                                      "text": "ITEMS",
+                                      "size": "sm",
+                                      "color": "#555555"
                                     },
                                     {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Chewing Gum",
-                                          "size": "sm",
-                                          "color": "#555555",
-                                          "flex": 0
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "$0.99",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "Bottled Water",
-                                          "size": "sm",
-                                          "color": "#555555",
-                                          "flex": 0
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "$3.33",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "separator",
-                                      "margin": "xxl"
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "margin": "xxl",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "ITEMS",
-                                          "size": "sm",
-                                          "color": "#555555"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "3",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "TOTAL",
-                                          "size": "sm",
-                                          "color": "#555555"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "$7.31",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "CASH",
-                                          "size": "sm",
-                                          "color": "#555555"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "$8.0",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
-                                    },
-                                    {
-                                      "type": "box",
-                                      "layout": "horizontal",
-                                      "contents": [
-                                        {
-                                          "type": "text",
-                                          "text": "CHANGE",
-                                          "size": "sm",
-                                          "color": "#555555"
-                                        },
-                                        {
-                                          "type": "text",
-                                          "text": "$0.69",
-                                          "size": "sm",
-                                          "color": "#111111",
-                                          "align": "end"
-                                        }
-                                      ]
+                                      "type": "text",
+                                      "text": "3",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
                                     }
                                   ]
                                 },
                                 {
-                                  "type": "separator",
-                                  "margin": "xxl"
+                                  "type": "box",
+                                  "layout": "horizontal",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "TOTAL",
+                                      "size": "sm",
+                                      "color": "#555555"
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "$7.31",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
+                                    }
+                                  ]
+                                },
+                                {
+                                  "type": "box",
+                                  "layout": "horizontal",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "CASH",
+                                      "size": "sm",
+                                      "color": "#555555"
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "$8.0",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
+                                    }
+                                  ]
+                                },
+                                {
+                                  "type": "box",
+                                  "layout": "horizontal",
+                                  "contents": [
+                                    {
+                                      "type": "text",
+                                      "text": "CHANGE",
+                                      "size": "sm",
+                                      "color": "#555555"
+                                    },
+                                    {
+                                      "type": "text",
+                                      "text": "$0.69",
+                                      "size": "sm",
+                                      "color": "#111111",
+                                      "align": "end"
+                                    }
+                                  ]
                                 }
                               ]
+                            },
+                            {
+                              "type": "separator",
+                              "margin": "xxl"
                             }
-                          }
-                    },
-                  }
+                          ]
+                        }
+                      }
+                }
                 ctx.body = echo;
                 return client.replyMessage(e.replyToken, echo);
 
