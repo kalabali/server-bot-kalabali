7cd779809d6c84ba5d906793a1d985bac155bf08
imdbsd <budisuryadarma0407@gmail.com>
2018-09-24 12:52:14 +0800

    update handle

--- service/handle.js
+++ service/handle.js
@@ -4,7 +4,7 @@ const comand = [
 function incoming(event){
     let response;
     if(event.type === 'message' || event.message.type === 'text'){
-        if(event.message.text.indexOf("#") !== -1 && command.indexOf(event.message.text) !== -1){ //check if user imputting available command
+        if(event.message.text.indexOf("#") !== -1 && comand.indexOf(event.message.text) !== -1){ //check if user imputting available command
             response = {
                 type: 'text',
                 text: "command is: " + event.message.text
